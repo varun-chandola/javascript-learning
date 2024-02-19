8eb381320df1157424df837dbfc729a9a8db7e2a
Varun Chandola <139681679+varun-chandola@users.noreply.github.com>
2024-02-19 03:41:04 +0000

    objects basics

--- 03_Objects/1_objects_basics.js
+++ 03_Objects/1_objects_basics.js
@@ -1,10 +1,12 @@
 // 2 ways of making objects 
 // 1. Literals , 2. Constructors
 // Constructor declaration makes singleton object
-// singleton -> Object.create
+// singleton -> Object.create()
 
-// object Literals
-const userObject = {
+// 1. object Literals
+// object creates key : value pairs
+// each key is used as string in objects
+const user = {
     // key : value
     name : 'varun',
     age : 22,
@@ -12,26 +14,46 @@ const userObject = {
     location : 'haldwani',
     isLoggedIn : false,
     email : 'varun7122001@gmail.com',
-    /* important */ "College Name" : 'Graphic Era'
+    "College Name" : 'Graphic Era' /* important */
 }
-userObject.name = 'varun chandola'
-// different ways to access memebers of the objects
-console.log(userObject.email);
-console.log(userObject['email']);  // more correct manner
-// console.log(userObject.College Name); // that why "key": notation is good
-console.log(userObject["College Name"]);
 
-userObject.greetings = function(){
+user.name = 'Varun Chandola'
+// different ways to access memebers of the objects
+console.log(user.email);  // varun7122001@gmail.com
+console.log(user['email']);  // varun7122001@gmail.com
+// if we use . to access keys / values of objects so we cannot access the College Name key in the object user . that is why we use ObjectName["KeyName"] to access
+
+// more correct manner
+// console.log(user.College Name); // error
+// that why "key": notation is good
+console.log(user["College Name"]); // Graphic Era
+
+// // how to use Symbol as key in object
+const symbolKey = Symbol("Key1");
+const myDetail = {
+  myName: "Varun",
+  myAge: 22,
+  myYOB: 2001,
+  symbolKey: "key1", // this is string data type
+  [symbolKey]: "Key1", // [key] : value // is syntax to use symbol is objs
+};
+console.log(myDetail["symbolKey"]) // key1 // but it access string variable named as symbolKey
+console.log(typeof myDetail.symbolKey) // String // but we need Symbol
+
+console.log(myDetail[symbolKey]); // key1 // that how we access Symbol in objs
+
+
+user.greetings = function(){
     console.log("greetings 1");
 }
 
-userObject.greetingsTwo = ()=>{
-    console.log(`greetings 2 , ${userObject.name}`);
+user.greetingsTwo = function(){
+    console.log(`greetings 2 , ${user.name}`);
+    //greetings 2 , Varun Chandola
+    // another way 
+    console.log(`greetings 2 , ${user["name"]}`)
+    //greetings 2 , Varun Chandola
 }
 
-userObject.greetingsThree = function(){
-    console.log(`greetings 3 , ${this.name}`);
-}
-console.log(userObject.greetings())
-console.log(userObject.greetingsTwo())
-console.log(userObject.greetingsThree());
+console.log(user.greetings())
+console.log(user.greetingsTwo())
