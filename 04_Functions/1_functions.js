0c54f7e1bd5e67bf15758e576570c68eac2eebdc
Varun Chandola <139681679+varun-chandola@users.noreply.github.com>
2024-02-20 04:23:19 +0000

    functions in js

--- 04_Functions/1_functions.js
+++ 04_Functions/1_functions.js
@@ -18,3 +18,72 @@ const res = restContinued(1,2,3,4,5,6,7,8,9,10)
 console.log(res)
 // val1 = 1 , val2 = 2 ,val3 = 3
 //  ...restValues = [4, 5,  6, 7,8, 9, 10] 
+
+
+
+// ...........
+function displayName() {
+    console.log("V")
+    console.log("A")
+    console.log("R")
+    console.log("U")
+    console.log("N")
+}
+
+// displayName ----------> function reference
+// displayName() -----------> function execute
+displayName()
+
+function userLogin(userName){
+    if(userName === undefined){
+        console.log("Enter Valid Username")
+        return;
+    }
+    return `${userName} just logged in` ;
+}
+
+console.log(userLogin("Varun")) // Varun just logged in
+console.log(userLogin()) // enter valid username
+
+function userLoginGreet(username = "DefaultName"){ // if undefined userLoginGreet() => DefaultName
+    if(!username){ // for "" => userLoginGreet("")
+        console.log("Enter valid username")
+        return;
+    }
+    return `${username} just logged in`;
+}
+
+console.log(userLoginGreet("Varun")) // Varun just loggged in
+console.log(userLoginGreet("")) // Enter Valid Username
+console.log(userLoginGreet()) // DefaultName just logged in
+
+
+
+// 2 . passing objects in function
+const detail = {
+    name : "Varun" , 
+    age : 22 , 
+    city : "haldwani"
+}
+
+function handleObject(anyObj){
+    return `${anyObj.name} , age ${anyObj.age} , from ${anyObj.city}` ;
+}
+
+console.log(handleObject(detail)) // Varun , age 22 , from haldwani
+
+console.log(handleObject({
+    name : "abc" , 
+    age : 20 , 
+    city : "delhi"
+}))
+
+
+//3.  passing array in functions
+function returnSecondValue(anyArray){
+    return anyArray[1];
+}
+
+const myArr = [10,20,30,40,50];
+console.log(returnSecondValue(myArr)); // 20
+console.log(returnSecondValue([1,2,3,4,5,6])); // 2
