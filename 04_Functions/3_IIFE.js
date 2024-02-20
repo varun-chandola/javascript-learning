d27e950e74d80ff9dcebad62546352308fec3f49
Varun Chandola <139681679+varun-chandola@users.noreply.github.com>
2024-02-20 07:08:12 +0000

    IIFE -> Immediately Invoked Function Expression ()()

---
+++ 04_Functions/3_IIFE.js
@@ -0,0 +1,28 @@
+// IIFE -> Immediately Invoked Function Expression ()()
+
+function one(){
+    console.log("DB CONNECTED");
+}
+one(); // function call
+
+
+// IIFE
+(function two(){
+    // named IIFE
+    console.log("DB CONNECTED");
+})() ; // we have to add ; because this exectution will not stop itself
+// ()() this gets referenced and executed at the same time
+// passing vals
+// ()() // function definition and funciton call at the same time
+
+(function add(num1,num2){
+    console.log(num1+num2) // 30
+})(10,20); // we have to add ; because this exectution will not stop itself
+
+(function hello(name){
+    console.log(`hi ${name}`); // hi Varun
+})("Varun") ; // we have to add ; because this exectution will not stop itself
+
+((name)=>{
+    console.log(`hi , your name is ${name}`) //hi , your name is Varun
+})("Varun");
